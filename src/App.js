e438b06dea985a4a6ea79cb39e38347f59084603
Mujica Garcia <gmujica93@outlook.com>
2020-04-30 20:47:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// Components
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import { fetchData } from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData()
+        console.log(fetchedData);
+
+        this.setState({ 
+            data: fetchedData 
+        })
+        
+    }
+
+    render() {
+        const { data } = this.state
+
+        return (
+            <div className={styles.container}>
+                <Cards data={data} />
+                <Chart />
+                <CountryPicker />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cards = (props) => {
+    console.log('DATA FROM CARDS COMPONENT:', props);
+
+    
+    
+    return (
+        <h1>Cards</h1>
+    )
+}
+
+export default Cards
