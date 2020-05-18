24165aa324fb9ce3d42150ae56acaad1566bb88e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-18 02:15:09 +0000

    Deploy gmujica/Covid_19_Tracker to github.com/gmujica/Covid_19_Tracker.git:gh-pages

---
+++ precache-manifest.f2a0809debf5be0ee1f83ce0a1f0ed04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1ffa79ead35352aa6febdc7cc67335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba8b98a4544b8e5a5d7",
+    "url": "/static/css/main.6681d8b4.chunk.css"
+  },
+  {
+    "revision": "8922a142cd58fdcb2a05",
+    "url": "/static/js/2.4d2e6881.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.4d2e6881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba8b98a4544b8e5a5d7",
+    "url": "/static/js/main.8f781196.chunk.js"
+  },
+  {
+    "revision": "d404866ad3d0f2dff924",
+    "url": "/static/js/runtime-main.1bdb6f6b.js"
+  }
+]);
